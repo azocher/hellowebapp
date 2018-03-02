8ccee8b105686fb31998583e745f11e2513d09a2
Anna Zocher <annazocher@Annas-MacBook-Pro.local>
2018-03-01 17:47:54 -0800

    new enviro/laptop and intro to tags/vars

--- collection/views.py
+++ collection/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	#defining our fun test variable
+	number = 6
+
+	#passing that fun variable to our view
+	return render(request, 'index.html', {
+		'number' : number,
+	})
