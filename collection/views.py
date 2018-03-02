03b1a33be4ed12dc24252bd6a39d3e44b2c30c5f
Anna Zocher <annazocher@Annas-MacBook-Pro.local>
2018-03-01 21:19:28 -0800

    added views to take data from our model and display in our template

--- collection/views.py
+++ collection/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request):
 	#defining our fun test variable
 	number = 6
-
+	things = Thing.objects.all()
 	#passing that fun variable to our view
 	return render(request, 'index.html', {
 		'number' : number,
+		'things' : things,
 	})
