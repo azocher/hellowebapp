c2470fd7a7d119c329460fc76dbb725a11ff068c
Anna Zocher <annazocher@Annas-MacBook-Pro.local>
2018-03-01 22:01:25 -0800

    edit thing view, and edit database entries from frontend via form

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from collection.forms import ThingForm
 from collection.models import Thing
 
 # Create your views here.
@@ -20,3 +21,29 @@ def thing_detail(request, slug):
 	return render(request, 'things/thing_detail.html', {
 		'thing': thing,
 	})
+
+def edit_thing(request, slug):
+	# grab our object
+	thing = Thing.objects.get(slug=slug)
+
+	# set the form we are using
+	form_class = ThingForm
+
+	#if coming to this view anyway but submitting a form, reject
+	if request.method == 'POST':
+
+		# grab the data from the submissions and apply to forms
+		form = form_class(data=request.POST, instance=thing)
+
+		if form.is_valid():
+			# save the form data
+			form.save()
+			return redirect('thing_detail', slug=thing.slug)
+
+	else:
+		form = form_class(instance=thing)
+
+		return render(request, 'things/edit_thing.html', {
+			'thing': thing,
+			'form': form,
+		})
