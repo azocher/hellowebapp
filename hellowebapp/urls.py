0e6c54038f53151f063b6d6afabb034ac2c4399d
Anna Zocher <annazocher@Annas-MacBook-Pro.local>
2018-03-04 23:03:24 -0800

    added auto redirects for base urls

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import (
+	TemplateView,
+	RedirectView
+)
 from collection import views
 from django.contrib.auth.views import (
 	password_reset,
@@ -14,8 +17,12 @@ urlpatterns = [
 	url(r'^$', views.index, name="home"),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, name='thing_detail'),
+	url(r'^things/$', RedirectView.as_view(pattern_name='browse', permanent=True)),
+	url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, name='thing_detail'),
     url(r'^things/(?P<slug>[-\w]+)/edit/$', views.edit_thing, name='edit_thing'),
+	url(r'^browse/$', RedirectView.as_view(pattern_name="browse", permanent=True)),
+	url(r'^browse/name/$', views.browse_by_name, name="browse"),
+	url(r'^browse/name/(?P<initial>[-\w]+)/$', views.browse_by_name, name="browse_by_name"),
 	url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
 	url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
 	url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
